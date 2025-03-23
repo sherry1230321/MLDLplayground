d67817fd324ae6e860ac67ea3529ab739bae8c32
sherry1230321 <sherryanncoulter2023@gmail.com>
2025-03-23 13:40:52 -0500

    Create Logistic_Regression.py

---
+++ supervised/Logistic_Regression.py/Logistic_Regression.py
@@ -0,0 +1,19 @@
+# logistic_regression.py
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+# Load dataset
+data = datasets.load_iris()
+X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.3, random_state=42)
+
+# Train Logistic Regression model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Evaluation
+print(classification_report(y_test, y_pred))
