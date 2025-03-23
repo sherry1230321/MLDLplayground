fb4afd540a0f548baadf1fb8cdf78e80c547953a
sherry1230321 <sherryanncoulter2023@gmail.com>
2025-03-23 13:38:04 -0500

    Create random_forest.py

---
+++ supervised/random_forest.py/random_forest.py
@@ -0,0 +1,19 @@
+# random_forest.py
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+
+# Load dataset
+data = datasets.load_iris()
+X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.3, random_state=42)
+
+# Train Random Forest model
+model = RandomForestClassifier(n_estimators=100)
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Evaluation
+print(classification_report(y_test, y_pred))
